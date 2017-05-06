b29c795b4cadaa5da4155943f1587acb77564aa7
Zach Behnke <ztbehnke62@gmail.com>
2017-05-06 13:20:26 -0700

    Added basis for yelp slash command.

--- api/resolver.js
+++ api/resolver.js
@@ -1,23 +1,26 @@
 var key = require('../utils/key');
 var sync = require('synchronize');
 var request = require('request');
-var _ = require('underscore');
-var createTemplate = require('../utils/template.js').resolver;
+var createTemplate = require('../utils/template');
 
 // The API that returns the in-email representation.
 module.exports = function(req, res) {
   var term = req.query.text.trim();
-  handleSearchString(term, req, res);
+
+  if (/^https:\/\/www\.yelp\.com\/biz\/\S+/.test(term)) {
+      handleSearchId(term.replace(/^https:\/\/www\.yelp\.com\/biz\//, ''), req, res);
+  } else {
+    handleSearchString(term, req, res);
+  }
 };
 
-function handleSearchString(term, req, res) {
+function handleSearchId(id, req, res) {
   var response;
   try {
     response = sync.await(request({
-      url: 'http://api.soundcloud.com/resolve',
-      qs: {
-        url: term,
-        client_id: key
+      url: 'https://api.yelp.com/v3/businesses/' + encodeURIComponent(id),
+      auth: {
+        'bearer': key
       },
       gzip: true,
       json: true,
@@ -29,6 +32,50 @@ function handleSearchString(term, req, res) {
   }
 
   res.json({
-    body: createTemplate(response.body)
+    body: createTemplate.resolver(response.body)
+  });
+}
+
+function handleSearchString(term, req, res) {
+  var delimIndex = term.indexOf(' in ');
+
+  if (delimIndex !== -1) {
+    var foodTerm = term.slice(0, delimIndex).trim();
+    var cityTerm = term.slice((foodTerm + ' in ').length).trim();
+  }
+
+  if (!foodTerm || !cityTerm) {
+    return;
+  }
+
+  var response;
+  try {
+    response = sync.await(request({
+      url: 'https://api.yelp.com/v3/businesses/search',
+      qs: {
+        term: foodTerm,
+        location: cityTerm,
+        radius: 25000, //Meters
+        limit: 1
+      },
+      auth: {
+        'bearer': key
+      },
+      gzip: true,
+      json: true,
+      timeout: 10 * 1000
+    }, sync.defer()));
+  } catch (e) {
+    res.status(500).send('Error');
+    return;
+  }
+
+  if (response.statusCode !== 200 || !response.body) {
+    res.status(500).send('Error');
+    return;
+  }
+
+  res.json({
+    body: createTemplate.resolver(response.body.businesses[0])
   });
 }
--- utils/key.js
+++ utils/key.js
@@ -1,2 +1,2 @@
-// Soundcloud API Key
-module.exports = 'eb6df903547f8123e3cb79e5429a0999';
+//Yelp API key
+module.exports = 'ykvMVWqqhLFGxiiwMyT63za30OWBoUKfbmr2Ctw8TAI-CO-TTHeAdtTJot8EagtVTNniPcGhTJkmnqzrjRuG_SY1sXs6_pOLVBB14ZysKPt5LIlDM1fk0cMDnHP4WHYx'
